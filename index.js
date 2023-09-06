a88ef12cf870dfea065c10d695a809bcdcb0aabe
Danil Zasypkin <d.zasypkin@tinkoff.ru>
2023-09-06 18:10:16 +0500

    modify

--- index.js
+++ index.js
@@ -3,53 +3,56 @@ const github = require('@actions/github');
 
 const main = async () => {
 
-  const repoToken = core.getInput('repo-token', { required: true });
+  const repoToken = core.getInput('repo-token', {required: true});
   const client = github.getOctokit(repoToken);
-  const { payload } = github.context;
+  const {payload} = github.context;
   const existingMemberMessage = core.getInput('existingMemberMessage')
 
   try {
-    const { INVITE_TOKEN } = process.env;
+    const {INVITE_TOKEN} = process.env;
 
     if (!INVITE_TOKEN) {
       return core.setFailed('ENV required and not supplied: INVITE_TOKEN');
     }
     const octokit = github.getOctokit(INVITE_TOKEN);
 
-    const inviteeId = payload.issue.user.id;
     const currentLabel = payload.label.name;
 
-    const org = core.getInput('organization', { required: true });
-    const label = core.getInput('label', { required: true });
+    const org = core.getInput('organization', {required: true});
+    const label = core.getInput('label', {required: true});
     const comment = core.getInput('comment');
+    const role = core.getInput('role', {required: true});
 
-    if (currentLabel === label) {
-      try {
-        await octokit.orgs.checkMembership({
-          org,
-          username: payload.issue.user.login,
-        });
-      } catch (error) {
-        await octokit.orgs.createInvitation({
-          org,
-          invitee_id: inviteeId,
-        });
-        core.info('Invitation sent successfully 🎉🎉');
+    core.info(`${ payload.issue.body }`);
 
-        await client.issues.createComment({
-          owner: payload.repository.owner.login,
-          repo: payload.repository.name,
-          issue_number: payload.issue.number,
-          body: comment,
-        });
+    try {
+      await octokit.orgs.checkMembership({
+        org,
+        username: payload.issue.user.login,
+      });
+    } catch (error) {
 
-        await client.issues.update({
-          owner: payload.repository.owner.login,
-          repo: payload.repository.name,
-          issue_number: payload.issue.number,
-          state: 'closed',
-        });
-      }
+      await octokit.teams.addOrUpdateMembershipForUserInOrg({
+        org: org,
+        team_slug: currentLabel,
+        username: 'USERNAME',
+        role: role,
+      });
+      core.info('Invitation sent successfully 🎉🎉');
+
+      await client.issues.createComment({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        issue_number: payload.issue.number,
+        body: comment,
+      });
+
+      await client.issues.update({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        issue_number: payload.issue.number,
+        state: 'closed',
+      });
     }
   } catch (error) {
 
